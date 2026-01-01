e3975437cff94178578d7b71e2b01e15ae23b7e0
RootNode404 <elijahpayne79@gmail.com>
2026-01-02 09:28:34 +1100

    Compleate function rewite (again). Improved logic, no visual change.

--- main.py
+++ main.py
@@ -1,128 +1,169 @@
 import curses
 import time
 
+# Helper function to align text
+def center(terminal, text):
+
+    h, w = terminal.getmaxyx()
+    x = h // 2
+    y = (w // 2) - (len(text) // 2)
+
+    return x, y
+
+# Function to print results to terminal
+def results(terminal, errors, start_time, end_time, text):
+
+    # Calculate wdm
+    characters = len(text)
+    total_time = end_time - start_time
+    msg = ""
+
+    wpm = f"Words Per Minute -> {round((characters / 5) * (60 / total_time) - errors, 2)}"
+    accuracy = ((characters - errors) / characters) * 100
+
+
+    X_CENTER, Y_CENTER = center(terminal, wpm)
+
+    # Clear the terminal and print results
+    terminal.clear()
+    terminal.addstr(X_CENTER - 6, Y_CENTER, wpm)
+
+    # Message if the user gets -percentage
+    if accuracy < 0:
+        msg = " bruh you kinda suck"
+
+    terminal.addstr(X_CENTER - 5, Y_CENTER, f"Accuracy -> {round(accuracy, 2)}%{msg}")
+    X_CENTER, Y_CENTER = center(terminal, "Press ENTER to restart or Q to quit.")
+    terminal.addstr(X_CENTER + 3, Y_CENTER, f"Press ENTER to restart or Q to quit.")
+
+    terminal.refresh()
+    
+    key_pressed = terminal.getch()
+
+    if key_pressed == curses.KEY_ENTER:
+        return "restart"
+    elif key_pressed == ord("q"):
+        return "exit"
+
+# Function to handle key presses
+def handle_keypresses(state, key, text):
+
+    # Check if index is bigger than the leg of text
+    if state["index"] > len(text):
+        return True  # Finished
+    
+    # Check if key is a alphabetical letter or an ANSI one
+    if 32 <= key <= 126:
+        char = chr(key)
+    else:
+        return False
+    
+    # Get the expected index char
+    expected = text[state["index"]]
+
+    if char == expected:
+        if not state["started"]:
+            state["started"] = True
+            state["start_time"] = time.perf_counter()
+
+        state["index"] += 1
+        state["incorrect"] = False
+
+    elif char != expected:
+        state["errors"] += 1
+        state["incorrect"] = True
+
+    return state["index"] == len(text)
+
+# Function to handle ui drawing
+def draw_term(terminal, text, state):
+
+    # Clear screen to start off with
+    terminal.clear()
+    
+    # Get all 3 different states of characters
+    typed_chars = text[:state["index"]]
+    cursor_char = text[state["index"]:state["index"] + 1]
+    untyped_chars = text[state["index"] + 1:]
+
+    # Return cords to align text to center of terminal
+    x, y = center(terminal, text)
+    
+    # Print typed characters
+    terminal.addstr(x, y, typed_chars, curses.color_pair(1))
+
+    # If cursor char present, print it to the terminal
+    if cursor_char:
+
+        if state["incorrect"] == True:
+            terminal.addstr(x, y + len(typed_chars), cursor_char, curses.color_pair(4))
+
+        elif state["incorrect"] == False:
+            terminal.addstr(x, y + len(typed_chars), cursor_char, curses.color_pair(3))
+    
+    # Print untyped chars to terminal, only add 1 to y if cursor is present
+    terminal.addstr(x, y + len(typed_chars) + (1 if cursor_char else 0), untyped_chars, curses.color_pair(2))
+
+    # Finally refresh the screen
+    terminal.refresh()
+
+# Main-loop to handle all main-loopy stuff
 def main_loop(terminal):
 
-    # Helper function to align text
-    def align_text(text):
-
-        h, w = terminal.getmaxyx()
-        x = h // 2
-        y = (w // 2) - (len(text) // 2)
-
-        return x, y
-
-    # Function to print results to terminal
-    def results(errors, start_time, end_time):
-
-        # Calculate wdm
-        words = len(text.split())
-        characters = len(text)
-        total_time = end_time - start_time
-
-        wpm = f"Words Per Minute -> {round((characters / 5) * (60 / total_time), 2)}"
-        accuracy = ((characters - errors) / characters) * 100
-
-
-        X_CENTER, Y_CENTER = align_text(wpm)
-
-        # Clear the terminal and print results
-        terminal.clear()
-        terminal.addstr(X_CENTER - 8, Y_CENTER, wpm)
-        terminal.addstr(X_CENTER - 7, Y_CENTER, f"Accuracy -> {round(accuracy, 2)}%")
-        X_CENTER, Y_CENTER = align_text("Press ENTER to restart or Q to quit.")
-        terminal.addstr(X_CENTER + 3, Y_CENTER, f"Press ENTER to restart or Q to quit.")
-
-        terminal.refresh()
-
-        if terminal.getch() == curses.KEY_ENTER:
-            curses.wrapper(main_loop)
-        elif terminal.getch() == ord("q"):
-            exit()
-
+    # Master state index thingo
+    state = {
+        "index": 0,
+        "errors": 0,
+        "started": False,
+        "start_time": None,
+        "incorrect": False,
+    }
+    
     text = "The quick brown fox jumps over the lazy dog"
 
-    terminal = curses.initscr()
+    terminal = curses.initscr() # Dosen't need to be here but need it for vscode function autocompleations
     curses.curs_set(0)
-    curses.start_color()
-    curses.use_default_colors()
     
 
     # Set some color
+    curses.start_color()
+    curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_GREEN, -1) # Typed letters
     curses.init_pair(2, curses.COLOR_WHITE, -1) # Untyped letters
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)  # Cursor
     curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_RED)  # Cursor incorrect
 
     # Print starting text
-    X_CENTER, Y_CENTER = align_text(text)
+    X_CENTER, Y_CENTER = center(terminal, text)
     terminal.addstr(X_CENTER, Y_CENTER, text, curses.color_pair(2))
 
     # Loop to handel typing
-    current_index = 0
-    key_shifted_pressed = "=" # fix me
-    errors = 0
-    while current_index < len(text):
-        
-        # Get the key pressed
-        key_pressed = terminal.getch()
-        
-        # Start stopwatch
-        if current_index == 0:
-            start_time = time.perf_counter()
+    finished = False
+    while finished == False:
 
-        # If the key is a shift key then wait for the next key press
-        if key_pressed == curses.KEY_SBEG:
-            key_shifted_pressed = terminal.getch()
-            shift_pressed = True
-    
-        # If key = current char or if shifted cahracter and shift pressed is true
-        if (key_pressed == ord(text[current_index])) or (str(ord(key_shifted_pressed)).upper() == ord(text[current_index]) and (shift_pressed == True )):
+        # Get pressed key
+        pressed_key = terminal.getch()
 
-            # After keypressed current_index becomes typed
-            current_index += 1
-            
-            # Get all the typed chars relative to the index
-            typed_chars = text[:current_index]
-
-            # Only move cursor along if there is a next character
-            if current_index < len(text):
-                current_cursor_char = text[current_index]
-                not_typed_chars = text[current_index + 1:]
-                terminal.clear()
-            else:
-                current_cursor_char = ""
-                not_typed_chars = ""
-
-            
-            # Typed Letters
-            terminal.addstr(X_CENTER, Y_CENTER, typed_chars, curses.color_pair(1))
-
-            # Only show cursor if there is a new character to go to
-            if current_cursor_char:
-                terminal.addstr(X_CENTER, Y_CENTER + len(typed_chars), current_cursor_char, curses.color_pair(3))
-            
-            # Untyped characters
-            terminal.addstr(X_CENTER, Y_CENTER + len(typed_chars) + 1, not_typed_chars, curses.color_pair(2))
-            
-
-        
-        elif key_pressed != ord(text[current_index]):
-            terminal.addstr(X_CENTER, Y_CENTER + len(typed_chars), current_cursor_char, curses.color_pair(4))
-            errors += 1
-
-
-        # Finally push it all to the display
-        terminal.refresh()
+        finished = handle_keypresses(state, pressed_key, text)
+        draw_term(terminal, text, state)
 
     
     # After while loop exits stop the timer
     end_time = time.perf_counter()
 
-    results(errors, start_time, end_time)
+    return results(terminal, state["errors"], state["start_time"], end_time, text)
 
 
 
 
 # Encase main loop in a wrapper to catch error and handle exiting properly
-curses.wrapper(main_loop)
+# Encase it in a while loop
+status = "restart"
+while status == "restart": 
+    
+    status = curses.wrapper(main_loop)
+
+    if status == "exit":
+        break
+    else:
+        status = "restart"
