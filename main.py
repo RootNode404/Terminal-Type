3767808fb0b177fae6a3ecb97fd4fd4c6fc7c62d
RootNode404 <elijahpayne79@gmail.com>
2026-01-01 12:55:11 +1100

    Complete rewrite. Now have cursor and color!

--- main.py
+++ main.py
@@ -1,78 +1,53 @@
 import curses
-import time
 
-def str_index(string, index):
+def main_loop(terminal):
 
-    return string[:index] + string[index].upper() + string[index + 1:]
+    text = "the quick brown fox jumps over the lazy dog"
 
-def add_char(string, index, char):
-
-    return string[:index] + char + string[index:]
-
-
-# Colors class
-class c:
-    B = '\033[94m'  # Blue
-    C = '\033[96m'  # Cyan
-    G = '\033[92m'  # Green
-    Y = '\033[93m'  # Yellow
-    R = '\033[91m'  # Red
-    E = '\033[0m'   # End color
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    HEADER = '\033[95m'
-
-# Wrapper for whole app to catch errors and handle them without leaving the terminal in a unusable state
-def main(terminal):
-
-
-    # Setup terminal and hide terminal curser
- 
     terminal = curses.initscr()
     curses.curs_set(0)
+    curses.noecho()
+    curses.start_color()
+    curses.use_default_colors()
 
-    # Clear the screen
-    terminal.clear()
+    curses.init_pair(1, curses.COLOR_RED, -1)
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    text = "the quick brown fox jumped over the lazy dog"
-
-    # Get terminal dimentions, then math to get center with text
-    SCREEN_H, SCREEN_W = terminal.getmaxyx()
-    X_CENTER = SCREEN_H // 2
-    Y_CENTER = (SCREEN_W // 2) - (len(text) // 2)
-
-    # Print words
-    terminal.addstr(X_CENTER, Y_CENTER, text)
-
-     
-    index = 0
-    for char in text:
-               
-        while True:
-
-            # Get key input from user
-            while True:
-                key = terminal.getch()
-                if key: break
-
-            # Check if key pressed is the next one in the string
-            if key == ord(char):
-                
-                text = str_index(text, index)
-            
-                terminal.addstr(X_CENTER, Y_CENTER, text)
-                index += 1 
-        
-                # Refresh screen
-                terminal.refresh()
-                break
-    
-
-    # Wait for key before exiting
-    terminal.getch()
-    
-
-    
+    # Print starting text
+    terminal.addstr(0, 1, text, curses.color_pair(1))
 
 
-curses.wrapper(main)
+    current_index = 0
+    while current_index < len(text):
+
+        key_pressed = terminal.getch()
+
+        if key_pressed == ord(text[current_index]):
+
+            # After keypressed current_index becomes typed
+            current_index += 1
+
+            typed_chars = text[:current_index]
+
+            # If not finished show next char as curser
+            if current_index < len(text):
+                cursor_char = text[current_index]
+                not_typed_chars = text[current_index + 1:]
+            else:
+                cursor_char = ""
+                not_typed_chars = ""
+
+            terminal.clear()
+
+            terminal.addstr(0, 1, typed_chars, curses.color_pair(2))
+
+            if cursor_char:
+                terminal.addstr(0, 1 + len(typed_chars), cursor_char, curses.color_pair(3))
+
+            terminal.addstr(0, 1 + len(typed_chars) + 1, not_typed_chars, curses.color_pair(1))
+
+            terminal.refresh()
+
+
+curses.wrapper(main_loop)
